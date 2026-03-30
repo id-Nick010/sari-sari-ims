1f4af168a2dbc2cc16c253f0f07d74653345617e
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2026-03-30 23:46:00 +0800

    feat: finished adding the greeting component w/ style

--- app/inventory.tsx
+++ app/inventory.tsx
@@ -1,6 +1,9 @@
 import GreetingBar from "@/src/components/greeting-bar";
+import VarColors from "@/src/theme/colors";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const colorStyle = VarColors;
 
 export default function InventoryScreen() {
   return (
@@ -15,5 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
+    backgroundColor: colorStyle.yellow.c10,
   },
 });
--- src/components/greeting-bar.tsx
+++ src/components/greeting-bar.tsx
@@ -1,17 +1,42 @@
 import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import VarColors from "../theme/colors";
+import VarContainers from "../theme/containers";
 import VarTypo from "../theme/typography";
+
+const contStyle = VarContainers;
+const typeStyle = VarTypo;
+const colorStyle = VarColors;
 
 export default function GreetingBar() {
   return (
     <>
       <View style={styles.container}>
         <View style={{ flex: 2 }}>
-          <Text style={styles.h1}>AGAO Sari-Sari Store Inventory</Text>
+          <Text style={styles.h1}>
+            AGAO Sari-Sari Store{" "}
+            <Text style={{ color: colorStyle.secondary.c500 }}>Inventory</Text>
+          </Text>
           <Text style={styles.p}>Wednesday, March 4, 2026</Text>
         </View>
+        <TouchableOpacity style={styles.langBtn}>
+          <Ionicons
+            name="language-outline"
+            size={25}
+            style={{ color: colorStyle.neutral.c700 }}
+          />
+          <Text
+            style={{ ...typeStyle.body.b3, color: colorStyle.neutral.c700 }}
+          >
+            Eng
+          </Text>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.notifBtn}>
-          <Ionicons name="notifications-outline" size={25} />
+          <Ionicons
+            name="notifications-outline"
+            size={25}
+            style={{ color: colorStyle.neutral.c700 }}
+          />
         </TouchableOpacity>
       </View>
     </>
@@ -24,15 +49,29 @@ const styles = StyleSheet.create({
     maxHeight: 100,
     paddingInline: 20,
     paddingBlock: 10,
+    gap: contStyle.spacing.s4,
+    backgroundColor: colorStyle.yellow.c10,
   },
-  h1: { ...VarTypo.head.h7 },
-  p: { ...VarTypo.body.b3 },
+  h1: { ...typeStyle.head.h7, color: colorStyle.primary.c300 },
+  p: { ...typeStyle.body.b3, color: colorStyle.secondary.c300 },
   notifBtn: {
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
+    width: "4%",
     borderRadius: 5,
     padding: 4,
-    aspectRatio: 1,
+    marginBlock: 5,
+  },
+  langBtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    width: "6%",
+    borderRadius: 5,
+    padding: 4,
+    marginBlock: 5,
+    gap: 5,
   },
 });
