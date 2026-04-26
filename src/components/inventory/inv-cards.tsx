4fa00683e66f64e648e09be42e5f9ea8fb7619df
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2026-04-26 16:53:02 +0800

    feat: pagination for card inv functional

--- src/components/inventory/inv-cards.tsx
+++ src/components/inventory/inv-cards.tsx
@@ -1,7 +1,10 @@
 import { Product } from "@/src/models/Product";
 import VarColors from "@/src/theme/colors";
 import VarContainers from "@/src/theme/containers";
-import { FlatList, StyleSheet, View } from "react-native";
+import VarTypo from "@/src/theme/typography";
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import ProductCard from "./product-card";
 
 interface InvCardsProps {
@@ -10,11 +13,21 @@ interface InvCardsProps {
 }
 
 export default function InvCards({ data, onEditRefresh }: InvCardsProps) {
+  const itemsPerPage = 10;
+  const [page, setPage] = useState(1);
+
+  const paginatedData = data.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage,
+  );
+
+  useEffect(() => console.log("curr page: " + page), [page]);
+
   return (
     <View>
       <FlatList
-        data={data}
-        keyExtractor={(data, index) => index.toString()}
+        data={paginatedData}
+        keyExtractor={(item, index) => index.toString()}
         numColumns={3}
         renderItem={({ item }) => (
           <View style={styles.card}>
@@ -23,6 +36,65 @@ export default function InvCards({ data, onEditRefresh }: InvCardsProps) {
         )}
         contentContainerStyle={styles.container}
         columnWrapperStyle={{ justifyContent: "space-between" }}
+        ListFooterComponent={
+          <View style={styles.footerRow}>
+            <Text style={styles.selectedDetailText}>Showing 8 of 16 items</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 5,
+              }}
+            >
+              <Pressable
+                style={[styles.pageBtn, styles.inactivePageBtn]}
+                onPress={() => {
+                  setPage((prev) => prev - 1);
+                }}
+              >
+                <Ionicons
+                  style={styles.pageIcon}
+                  name="chevron-back-outline"
+                  size={18}
+                />
+              </Pressable>
+              {[...Array(Math.ceil(data.length / itemsPerPage))].map((_, i) => (
+                <Pressable
+                  style={[
+                    styles.pageBtn,
+                    page === i + 1
+                      ? styles.activePageBtn
+                      : styles.inactivePageBtn,
+                  ]}
+                  key={i}
+                  onPress={() => setPage(i + 1)}
+                >
+                  <Text
+                    style={[
+                      styles.pageText,
+                      page === i + 1
+                        ? styles.activePageText
+                        : styles.inactivePageText,
+                    ]}
+                  >
+                    {(i + 1).toString()}
+                  </Text>
+                </Pressable>
+              ))}
+              <Pressable
+                style={[styles.pageBtn, styles.inactivePageBtn]}
+                onPress={() => {
+                  setPage((prev) => prev + 1);
+                }}
+              >
+                <Ionicons
+                  style={styles.pageIcon}
+                  name="chevron-forward-outline"
+                  size={18}
+                />
+              </Pressable>
+            </View>
+          </View>
+        }
       />
     </View>
   );
@@ -40,5 +112,44 @@ const styles = StyleSheet.create({
     borderWidth: VarContainers.stroke.s1,
     borderRadius: 8,
     borderColor: VarColors.secondary.c100,
+  },
+  footerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingHorizontal: 20,
+  },
+  selectedDetailText: {
+    color: VarColors.neutral.c600,
+  },
+  pageBtn: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 35,
+    aspectRatio: 1 / 1,
+    borderRadius: VarContainers.radius.s5,
+  },
+  activePageBtn: {
+    backgroundColor: VarColors.primary.c300,
+    borderWidth: 0,
+  },
+  inactivePageBtn: {
+    backgroundColor: VarColors.neutral.c100,
+    borderColor: VarColors.neutral.c200,
+    borderWidth: VarContainers.stroke.s0,
+  },
+  pageIcon: {
+    color: VarColors.neutral.c700,
+  },
+  pageText: {
+    ...VarTypo.body.b3_m,
+  },
+  activePageText: {
+    color: VarColors.neutral.c100,
+  },
+  inactivePageText: {
+    color: VarColors.secondary.c500,
   },
 });
